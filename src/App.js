792870ce1188c4b6127bfb438a3143a0383c3f4d
Alvee <M.Alvee8141@gmail.com>
2019-08-19 05:49:09 -0700

    DONE

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from "react-player";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      inputValue : "",
+      url:""
+    }
+  }
+
+  handleChange = (event)=>{
+    this.setState({inputValue : event.target.value})
+  }
+
+  handleSubmit = (event) =>{
+    event.preventDefault();
+    this.setState({url: this.state.inputValue})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div>
+              <form onSubmit={this.handleSubmit}>
+                <input onChange={this.handleChange} style={{margin:"20px"}} className="form-control" type="text" placeholder="Input the vidoe url" />
+                <button style={{margin:"20px"}} className="btn btn-primary">PLAY VIDEO</button>
+              </form>
+            
+          </div>
+          <ReactPlayer url={this.state.url} controls={true} />
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
