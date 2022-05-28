252d98e7c6857217336479125f6696851e451f43
Ankit Tyagi <you@example.com>
2022-05-29 02:45:51 +0530

    Updates

---
+++ precache-manifest.9e952583c41039388e94bd8d22350461.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb29c66b040ee58649bf9063934a131e",
+    "url": "/namasteTube/index.html"
+  },
+  {
+    "revision": "3f9cb7598e4784ecbbb5",
+    "url": "/namasteTube/static/css/main.c53019a0.chunk.css"
+  },
+  {
+    "revision": "8e753d47a70fe2de14bd",
+    "url": "/namasteTube/static/js/2.093fd4c4.chunk.js"
+  },
+  {
+    "revision": "3f9cb7598e4784ecbbb5",
+    "url": "/namasteTube/static/js/main.02f25ce3.chunk.js"
+  },
+  {
+    "revision": "525822182bd22c0f4fae",
+    "url": "/namasteTube/static/js/runtime~main.9aaf03f3.js"
+  },
+  {
+    "revision": "5e93cacb5637307f911c0dc3cde21983",
+    "url": "/namasteTube/static/media/Logo.5e93cacb.jpg"
+  }
+]);
